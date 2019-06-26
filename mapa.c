24ab99f60bc55cf8bef169e54ad70aaa98dc4dea
Jonas Evangelista <jonas.eos@outlook.com>
2019-06-26 06:43:24 -0300

    Atualizaçao no mapa.c
    * Adicionado ponteiro *char e alocado de acordo com o tamanho de da
    constante RA
    
    * Adicionado release no programa principal para variavel ra

--- mapa.c
+++ mapa.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 const char RA[] = "19953835";
 
-int main() { return 0; }
+int main() {
+  char *ra = calloc(strlen(RA), sizeof(ra));
+  free(ra);
+  return 0;
+}
