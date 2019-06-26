db6e8fd7c996cfe3013de3a350e8d70f48d11719
Jonas Evangelista <jonas.eos@outlook.com>
2019-06-26 06:59:43 -0300

    Atualizaçao no mapa.c procedimento exibirRA * Adicionado procedimento
    * Mostra todo o conteudo do ponteiro, sendo cada linha uma posiçao deste
    ponteiro informado

--- mapa.c
+++ mapa.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-const char RA[] = "19953835";
+const char RA[] = "199538356";
 
 void preencherPonteiro(char *__ptr) {
   int index;
@@ -11,9 +11,18 @@ void preencherPonteiro(char *__ptr) {
   }
 }
 
+//
+void exibirRa(const char *__ptr) {
+  int index;
+  for (index = 0; index < sizeof(__ptr); index++) {
+    printf("%i\n", __ptr[index]);
+  }
+}
+
 int main() {
   char *ra = calloc(strlen(RA), sizeof(ra));
   preencherPonteiro(ra);
+  exibirRa(ra);
   free(ra);
   return 0;
 }
