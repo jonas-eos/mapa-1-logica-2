cf17c0729e8f92f9de51224b06093b69950dec82
Jonas Evangelista <jonas.eos@outlook.com>
2019-06-26 06:54:12 -0300

    Atualizaçao Mapa.c Procedure preencherPonteiro * Adicionado procedure * Preenche um vetor com os dados do RA em cada posiçao individual

--- mapa.c
+++ mapa.c
@@ -4,8 +4,16 @@
 
 const char RA[] = "19953835";
 
+void preencherPonteiro(char *__ptr) {
+  int index;
+  for (index = 0; index < sizeof(__ptr); index++) {
+    *(__ptr + index) = RA[index] - '0';
+  }
+}
+
 int main() {
   char *ra = calloc(strlen(RA), sizeof(ra));
+  preencherPonteiro(ra);
   free(ra);
   return 0;
 }
